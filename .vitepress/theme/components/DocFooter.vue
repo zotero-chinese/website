105f3677cfb1a0794d8f67d48b8e748162f3834b
Northword <northword@outlook.com>
2024-05-07 13:11:27 +0800

    修改转换器页面许可协议为仓库协议

--- .vitepress/theme/components/DocFooter.vue
+++ .vitepress/theme/components/DocFooter.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useData } from "vitepress";
+const filePath = ref(useData().page.value.filePath);
+</script>
 
 <template>
+  <!-- License -->
   <div class="doc-before-footer-cc-block mt-10 text-sm font-medium">
     使用
     <a
+      v-if="filePath.match('translators/')"
+      class="footer-cc-link"
+      target="_blank"
+      href="https://github.com/l0o0/translators_CN?tab=AGPL-3.0-1-ov-file#readme"
+    >
+      AGPL-3.0
+    </a>
+    <a
+      v-else-if="filePath.match('styles/')"
+      class="footer-cc-link"
+      target="_blank"
+      href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh-hans"
+    >
+      CC BY-SA 4.0
+    </a>
+    <a
+      v-else
       class="footer-cc-link"
       target="_blank"
       href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh-hans"
