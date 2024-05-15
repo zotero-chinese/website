1e769a0347051b3a946c98391d8c30588fc7f5a0
Northword <northword@outlook.com>
2024-05-15 18:17:57 +0800

    fix: bump git changelog plugin

--- .vitepress/data/contributors.ts
+++ .vitepress/data/contributors.ts
@@ -19,8 +19,6 @@ export interface Creator extends WikiContributor {
   emailAliases?: string[];
 }
 
-const getAvatarUrl = (name: string) => `https://github.com/${name}.png`;
-
 export const contributors: Creator[] = [
   {
     name: "Northword",
@@ -34,22 +32,7 @@ export const contributors: Creator[] = [
     ],
   },
   ...(wikiContributors as Creator[]),
-].map<Creator>((c) => {
-  c.avatar = getAvatarUrl(c.username);
-  c.links = [
-    {
-      type: "github",
-      icon: "github",
-      link: `https://github.com/${c.username}`,
-    },
-  ];
-  if (!c.nameAliases) {
-    c.nameAliases = [];
-    c.nameAliases.push(c.name);
-  }
-  console.log(c);
-  return c as Creator;
-});
+];
 
 export const creatorNames = contributors.map((c) => c.name);
 export const creatorUsernames = contributors.map((c) => c.username || "");
