a9019e59c7b3fe476b641bdfa6d4aacc138565a7
Northword <northword@outlook.com>
2024-06-30 19:38:25 +0800

    feat: support mark syntax, fix: #60

--- .vitepress/config/markdown.config.ts
+++ .vitepress/config/markdown.config.ts
@@ -1,10 +1,12 @@
 import type { UserConfig } from 'vitepress'
 import { footnote } from '@mdit/plugin-footnote'
+import { mark } from '@mdit/plugin-mark'
 import { markdown as _markdown } from '../../src/wiki/.vitepress/config/markdown'
 
 export const markdown: UserConfig['markdown'] = {
   ..._markdown,
   config: (md) => {
     md.use(footnote)
+    md.use(mark)
   },
 }
