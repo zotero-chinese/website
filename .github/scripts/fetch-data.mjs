3e0c28d6d3ce3d0fcb3914d861952103e8420610
Northword <northword@outlook.com>
2024-07-05 15:01:46 +0800

    fix: fetch contributors image when build, fix: #69

--- .github/scripts/fetch-data.mjs
+++ .github/scripts/fetch-data.mjs
@@ -26,11 +26,43 @@ const data_list = [
   },
 ]
 
-const tasks = data_list.map(async (d) => {
+const tasks_data = data_list.map(async (d) => {
   const data = await (await fetch(d.remote_url)).json()
   fs.outputJSONSync(path.resolve(d.local_path), data)
   console.log(`Download ${d.local_path} success!`)
 })
+
+// Contributors images
+const contributors_list = [
+  {
+    local_path: 'src/public/contributors_zotero-chinese_wiki.svg',
+    remote_url:
+        'https://github.com/zotero-chinese/.github/blob/main/.github-contributors/zotero-chinese_wiki.svg',
+  },
+  {
+    local_path: 'src/public/contributors_zotero-chinese_zotero-plugins.svg',
+    remote_url:
+        'https://github.com/zotero-chinese/.github/blob/main/.github-contributors/zotero-chinese_zotero-plugins.svg.svg',
+  },
+  {
+    local_path: 'src/public/contributors_zotero-chinese_styles.svg',
+    remote_url:
+        'https://github.com/zotero-chinese/.github/blob/main/.github-contributors/zotero-chinese_styles.svg',
+  },
+  {
+    local_path: 'src/public/contributors_l0o0_translators_CN.svg',
+    remote_url:
+        'https://github.com/zotero-chinese/.github/blob/main/.github-contributors/l0o0_translators_CN.svg',
+  },
+]
+
+const tasks_contributors = contributors_list.map(async (d) => {
+  const data = await (await fetch(d.remote_url)).text()
+  fs.outputFileSync(path.resolve(d.local_path), data)
+  console.log(`Download ${d.local_path} success!`)
+})
+
+const tasks = [...tasks_data, ...tasks_contributors]
 
 Promise.all(tasks)
   .then(() => {
