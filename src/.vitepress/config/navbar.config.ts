fb57dde5859f61c5bcd52b24bf1b25666256e93c
Northword <northword@outlook.com>
2024-07-17 16:08:07 +0800

    fix: mv dev guide to nav root

--- src/.vitepress/config/navbar.config.ts
+++ src/.vitepress/config/navbar.config.ts
@@ -6,33 +6,28 @@ export const nav: DefaultTheme.NavItem[] = [
   { text: 'CSL 样式', link: '/styles/', activeMatch: '/styles/' },
   { text: '转换器', link: '/translators/', activeMatch: '/translators/' },
   {
-    text: '更多资源',
+    text: '开发者指南',
     items: [
       {
-        text: '开发指南',
-        items: [
-          {
-            text: '插件开发指南',
-            link: '/plugin-dev-guide/',
-          },
-          {
-            text: 'CSL 开发指南',
-            link: '/csl-dev-guide/',
-          },
-          {
-            text: 'Translator 开发指南',
-            link: '/translator-dev-guide/',
-          },
-        ],
+        text: '插件开发指南',
+        link: '/plugin-dev-guide/',
       },
       {
-        text: '最好的中文社区',
-        items: [
-          { text: '关于我们', link: '/about' },
-          { text: '贡献者名单', link: '/contributors' },
-          { text: '贡献指南', link: '/contributing/' },
-        ],
+        text: 'CSL 开发指南',
+        link: '/csl-dev-guide/',
       },
+      {
+        text: 'Translator 开发指南',
+        link: '/translator-dev-guide/',
+      },
+    ],
+  },
+  {
+    text: '社区',
+    items: [
+      { text: '关于我们', link: '/about' },
+      { text: '贡献者名单', link: '/contributors' },
+      { text: '贡献指南', link: '/contributing/' },
     ],
   },
 ]
