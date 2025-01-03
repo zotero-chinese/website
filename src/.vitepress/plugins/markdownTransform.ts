7deefd78767701bc57970f29020891c09c0d0673
Northword <northword@outlook.com>
2025-01-03 19:34:38 +0800

    Fix escape `<sup>[3](326-329)</sup>`

--- src/.vitepress/plugins/markdownTransform.ts
+++ src/.vitepress/plugins/markdownTransform.ts
@@ -64,7 +64,7 @@ export function MarkdownTransform(): Plugin {
 
         // escape: （张三，2008）<sup>[1](42)</sup>
         code = code.replaceAll(
-          /\[(\d+)\]\((\d*)\)/g,
+          /\[(\d+)\]\(([\d-]*)\)/g,
           '\\[$1\\]\\($2\\)',
         )
       }
