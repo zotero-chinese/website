64da9acd0c61b2b1366e58cbc662c1921f760882
Northword <northword@outlook.com>
2025-03-30 12:22:13 +0800

    fix: escaping of inline code blocks
    ref: https://github.com/vuejs/vitepress/discussions/3724

--- src/.vitepress/config/markdown.config.ts
+++ src/.vitepress/config/markdown.config.ts
@@ -1,12 +1,6 @@
 import type { UserConfig } from 'vitepress'
-import { footnote } from '@mdit/plugin-footnote'
-import { mark } from '@mdit/plugin-mark'
 import { markdown as _markdown } from '../../wiki/.vitepress/markdown'
 
 export const markdown: UserConfig['markdown'] = {
-  ..._markdown as Omit<UserConfig['markdown'], 'config'>,
-  config: (md) => {
-    md.use(footnote)
-    md.use(mark)
-  },
+  ..._markdown,
 }
